8cd60e697728710275fce9137ff569e14d2cada9
pedromsmendes <pedro.msoares.mendes@gmail.com>
2022-02-24 16:34:34 +0000

    Expand that damn layout to 100vh

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,18 +6,13 @@ import Box from '@mui/material/Box';
 
 import { styled } from '@mui/material';
 
-const LayoutContainer = styled('div')({
+import styles from './Layout.module.css';
+
+const LayoutContainerWithStyled = styled('div')({
+  width: '100vw',
+  height: '100vh',
   display: 'flex',
   flexDirection: 'column',
-  border: '1px solid red',
-  width: '100%',
-  height: '100%',
-});
-
-const ComponentContainer = styled('div')({
-  flexGrow: 1,
-  backgroundColor: 'gray',
-  border: '1px solid blue',
 });
 
 const Layout: NextPage = ({ children }) => {
@@ -25,13 +20,13 @@ const Layout: NextPage = ({ children }) => {
   console.log('🚀 ~ router', router);
 
   return (
-    <LayoutContainer>
-      <ComponentContainer>
+    <LayoutContainerWithStyled>
+      <Box sx={{ flexGrow: 1 }}>
         {children}
-      </ComponentContainer>
+      </Box>
 
-      <Box>footer</Box>
-    </LayoutContainer>
+      <Box className={styles.footer}>footer</Box>
+    </LayoutContainerWithStyled>
   );
 };
 
